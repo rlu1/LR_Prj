a1ad46c67754bbe4430c07ba4a25b2dc0782da0f
Run LU <run.lu@valeo.com>
2019-04-25 15:23:25 +0800

    Jenkinsfile.sh

---
+++ Jenkinsfile.sh
@@ -0,0 +1,12 @@
+node {
+ echo 'Hello world';
+   stage('Preparation') { 
+      echo 'Preparation';
+   }
+   stage('Build') {
+      echo 'build';
+   }
+   stage('Results') {
+      echo 'Results';
+   }
+}
